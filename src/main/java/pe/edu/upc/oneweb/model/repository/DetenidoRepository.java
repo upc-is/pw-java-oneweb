6f6a9a3a7aa92d35e13f412849f956ac0633829b
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-04-27 14:06:00 -0500

    Add method findAll and findById in DetenidoRepositoryImpl

--- src/main/java/pe/edu/upc/oneweb/model/repository/DetenidoRepository.java
+++ src/main/java/pe/edu/upc/oneweb/model/repository/DetenidoRepository.java
@@ -1,7 +1,10 @@
 package pe.edu.upc.oneweb.model.repository;
 
+import java.util.List;
+
 import pe.edu.upc.oneweb.model.entity.Detenido;
 
 public interface DetenidoRepository extends JpaRepository<Detenido, Integer> {	
 	Detenido findByDni(String dni) throws Exception;
+	List<Detenido> findByApellidos( String apellidos ) throws Exception;
 }
--- src/main/java/pe/edu/upc/oneweb/model/repository/impl/DetenidoRepositoryImpl.java
+++ src/main/java/pe/edu/upc/oneweb/model/repository/impl/DetenidoRepositoryImpl.java
@@ -1,11 +1,13 @@
 package pe.edu.upc.oneweb.model.repository.impl;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import pe.edu.upc.oneweb.model.entity.Detenido;
 import pe.edu.upc.oneweb.model.repository.DetenidoRepository;
@@ -20,39 +22,65 @@ public class DetenidoRepositoryImpl implements DetenidoRepository, Serializable 
 
 	@Override
 	public Integer insert(Detenido t) throws Exception {
-		// TODO Auto-generated method stub
+		// 
 		em.persist(t);		
 		return t.getId();
 	}
 
 	@Override
 	public Integer update(Detenido t) throws Exception {
-		// TODO Auto-generated method stub
+		// 
 		em.merge(t);
 		return t.getId();
 	}
 
 	@Override
 	public Integer delete(Detenido t) throws Exception {
-		// TODO Auto-generated method stub
+		// 
 		em.remove(t);		
 		return t.getId();
 	}
 
 	@Override
 	public List<Detenido> findAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// 
+		List<Detenido> detenidos = new ArrayList<>();
+		// Elaborando la sentencia JPQL 
+		TypedQuery<Detenido> query = em.createQuery("SELECT d FROM Detenido AS d", Detenido.class);
+		// Ejecuta el query y devuelve el resultado del query
+		detenidos = query.getResultList();
+		return detenidos;
 	}
 
 	@Override
 	public Detenido findById(Integer id) throws Exception {
+		// 
+		List<Detenido> detenidos = new ArrayList<>();
+		
+		// Elaborando la sentencia JPQL
+		// Opción 1: utilizando el setParameter
+		TypedQuery<Detenido> query = em.createQuery("SELECT d FROM Detenido d WHERE d.id = ?1", Detenido.class);
+		query.setParameter(1, id);
+		// Opción 2: Cuando el valor a buscar esta en una variable
+		// TypedQuery<Detenido> query = em.createQuery("SELECT d FROM Detenido d WHERE d.id = :id)", Detenido.class);
+		
+		// Ejecuta el query y devuelve el resultado del query
+		detenidos = query.getResultList();
+		
+		if( detenidos == null || detenidos.isEmpty() )
+			return new Detenido();	// return null;
+		else
+			return detenidos.get(0);
+	}
+
+	@Override
+	public Detenido findByDni(String dni) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Detenido findByDni(String dni) throws Exception {
+	public List<Detenido> findByApellidos(String apellidos) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
