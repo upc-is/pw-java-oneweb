6870c6f7926a62231295701aade52a8e559fcee3
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-05-02 17:59:54 -0500

    Done for View layer

--- src/main/java/pe/edu/upc/oneweb/controller/DetenidoController.java
+++ src/main/java/pe/edu/upc/oneweb/controller/DetenidoController.java
@@ -3,12 +3,20 @@ package pe.edu.upc.oneweb.controller;
 import java.io.Serializable;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import pe.edu.upc.oneweb.business.DetenidoBusiness;
 
 @Named
 @ViewScoped
 public class DetenidoController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	//@Inject
+	//private DetenidoBusiness detenidoBusiness;
+	
+	
 
 }
