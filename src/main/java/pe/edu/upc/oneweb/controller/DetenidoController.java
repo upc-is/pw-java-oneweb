0b84daf120e5a03e2b65fbda3ff712beea94ba94
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-05-02 17:17:56 -0500

    Adding jsf and primefaces

---
+++ src/main/java/pe/edu/upc/oneweb/controller/DetenidoController.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.oneweb.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class DetenidoController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
